792c252dcbd5b28f82e09a21c31c7aa35104a2e6
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-10-06 04:40:16 +0000

    RISC-V: Improve "bits undefined" diagnostics
    This commit improves internal error message
    "internal: bad RISC-V opcode (bits 0x%lx undefined): %s %s"
    to display actual unused bits (excluding non-instruction bits).
    
    gas/ChangeLog:
    
    	* config/tc-riscv.c (validate_riscv_insn): Exclude non-
    	instruction bits from displaying internal diagnostics.
    	Change error message slightly.

--- gas/config/tc-riscv.c
+++ gas/config/tc-riscv.c
@@ -1312,8 +1312,8 @@ validate_riscv_insn (const struct riscv_opcode *opc, int length)
   if (used_bits != required_bits)
     {
       as_bad (_("internal: bad RISC-V opcode "
-		"(bits 0x%lx undefined): %s %s"),
-	      ~(unsigned long)(used_bits & required_bits),
+		"(bits 0x%llx undefined or invalid): %s %s"),
+	      (unsigned long long)(used_bits ^ required_bits),
 	      opc->name, opc->args);
       return false;
     }
